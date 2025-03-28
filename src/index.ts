0f664d2571d7a8f23b063f2b3d3b98a0f1de8fe3
windbullet <2536810643@qq.com>
2025-03-28 10:57:35 +0800

    提交文件

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,22 @@ import { Context, Schema } from 'koishi'
 
 export const name = 'auto-self-recall'
 
-export interface Config {}
+export const reusable = true
 
-export const Config: Schema<Config> = Schema.object({})
+export interface Config {
+  time: number
+}
 
-export function apply(ctx: Context) {
-  // write your plugin here
+export const Config: Schema<Config> = Schema.object({
+  time: Schema.number()
+    .description("消息发出后过多久撤回（s）")
+    .required()
+})
+
+export function apply(ctx: Context, config: Config) {
+  ctx.on("send", (session) => {
+    ctx.setTimeout(async () => {
+      await session.bot.deleteMessage(session.channelId, session.messageId)
+    }, config.time * 1000)
+  })
 }
